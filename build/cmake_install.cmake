adb13007e03cb20d6edf89cd0d773b04281057c4
Iurii <iurii@Iuriis-MacBook-Air.local>
2026-03-10 17:35:07 +0300

    STL remeshing of pencil done

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/user/gmsh-project
+# Install script for directory: /Users/iurii/Documents/mipt/2sem/matprac/gmsh-project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -42,10 +37,16 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/iurii/Documents/mipt/2sem/matprac/gmsh-project/build/lab1_meshes/cmake_install.cmake")
+
+endif()
+
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/user/gmsh-project/build/install_local_manifest.txt"
+  file(WRITE "/Users/iurii/Documents/mipt/2sem/matprac/gmsh-project/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +62,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/user/gmsh-project/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/iurii/Documents/mipt/2sem/matprac/gmsh-project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- lab1_meshes/stl/main.cpp
+++ lab1_meshes/stl/main.cpp
@@ -1,78 +1,75 @@
 #include <set>
-#include <gmsh.h>
 #include <cmath>
-#include <vector>
+#include <gmsh.h>
 
-struct TorusParams {
-  double R;
-  double r;
-};
+int main(int argc, char **argv)
+{
+  gmsh::initialize();
 
-int CreateTorusProfile(const TorusParams &params) {
-  double R = params.R;
-  double r = params.r;
+  gmsh::model::add("lab1_meshes/std/t13");
 
-  // xz plane
-  int p1 = gmsh::model::occ::addPoint(R + r, 0.0,  0.0);
-  int p2 = gmsh::model::occ::addPoint(R,     0.0,  r  );
-  int p3 = gmsh::model::occ::addPoint(R - r, 0.0,  0.0);
-  int p4 = gmsh::model::occ::addPoint(R,     0.0, -r  );
+  try {
+    gmsh::merge("lab1_meshes/stl/pencil.stl");
+  } catch(...) {
+    gmsh::logger::write("Could not load STL mesh: bye!");
+    gmsh::finalize();
+    return 0;
+  }
 
-  int pc = gmsh::model::occ::addPoint(R, 0.0, 0.0);
-
-  int a1 = gmsh::model::occ::addCircleArc(p1, pc, p2);
-  int a2 = gmsh::model::occ::addCircleArc(p2, pc, p3);
-  int a3 = gmsh::model::occ::addCircleArc(p3, pc, p4);
-  int a4 = gmsh::model::occ::addCircleArc(p4, pc, p1);
-
-  int loopTag = gmsh::model::occ::addCurveLoop({a1, a2, a3, a4});
-  return loopTag;
-}
-
-std::vector<std::pair<int,int>> CreateTorusVolume(int profileLoopTag) {
-  int surfTag = gmsh::model::occ::addPlaneSurface({profileLoopTag});
-  std::vector<std::pair<int,int>> out;
-  gmsh::model::occ::revolve(
-    {{2, surfTag}},
-    0.0, 0.0, 0.0,
-    0.0, 0.0, 1.0,
-    2.0 * M_PI,
-    out
-  );
-  return out;
-}
-
-std::vector<std::pair<int,int>> CreateTorus(const TorusParams &params) {
-  int profileCurveTag = CreateTorusProfile(params);
-
-  return CreateTorusVolume(profileCurveTag);
-}
+  // We first classify ("color") the surfaces by splitting the original surface
+  // along sharp geometrical features. This will create new discrete surfaces,
+  // curves and points.
 
 
-void BuildMesh(double h) {
+  // Angle between two triangles above which an edge is considered as sharp:
+  double angle = 40;
+
+  // For complex geometries, patches can be too complex, too elongated or too
+  // large to be parametrized; setting the following option will force the
+  // creation of patches that are amenable to reparametrization:
+  bool forceParametrizablePatches = false;
+
+  // For open surfaces include the boundary edges in the classification process:
+  bool includeBoundary = true;
+
+  // Force curves to be split on given angle:
+  double curveAngle = 180;
+
+  gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary,
+                                      forceParametrizablePatches,
+                                      curveAngle * M_PI / 180.);
+
+  // Create a geometry for all the discrete curves and surfaces in the mesh, by
+  // computing a parametrization for each one
+  gmsh::model::mesh::createGeometry();
+
+  // Create a volume from all the surfaces
+  std::vector<std::pair<int, int> > s;
+  gmsh::model::getEntities(s, 2);
+  std::vector<int> sl;
+  for(auto surf : s) sl.push_back(surf.second);
+  int l = gmsh::model::geo::addSurfaceLoop(sl);
+  gmsh::model::geo::addVolume({l});
+
+  gmsh::model::geo::synchronize();
+
+  // We specify element sizes imposed by a size field, just because we can :-)
+  bool funny = false; // false;
+  
+  double h = 2.0;
+
   gmsh::option::setNumber("Mesh.CharacteristicLengthMin", h);
   gmsh::option::setNumber("Mesh.CharacteristicLengthMax", h);
 
-  gmsh::model::occ::synchronize();
+
   gmsh::model::mesh::generate(3);
-}
 
-int main(int argc, char **argv) {
-  gmsh::initialize();
-  gmsh::model::add("Torus");
+  gmsh::write("t4.msh");
 
-  TorusParams torus{4.0, 1.0};
-
-  auto created = CreateTorus(torus); 
-  
-  gmsh::model::occ::synchronize();
-  BuildMesh(0.6);
-
-
+  // Launch the GUI to see the results:
   std::set<std::string> args(argv, argv + argc);
   if(!args.count("-nopopup")) gmsh::fltk::run();
 
   gmsh::finalize();
-
   return 0;
 }
