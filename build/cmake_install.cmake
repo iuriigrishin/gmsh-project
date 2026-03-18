25dc56eae08051e14732309c5f1d7c49c61b19c0
Iurii Grishin <iurkagrishin187@gmail.com>
2026-03-18 22:17:11 +0300

    add hollow torus

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -40,6 +40,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/Users/iurii/Documents/mipt/2sem/matprac/gmsh-project/build/lab1_meshes/cmake_install.cmake")
+  include("/Users/iurii/Documents/mipt/2sem/matprac/gmsh-project/build/lab2_vtk/cmake_install.cmake")
 
 endif()
 
